d05fad6cb5f050ba425c09ab4e0d412d28bd2544
Azat31 <929312w@gmail.com>
2023-01-10 23:34:51 -0500

    finish javascript fundamentals

---
+++ javascript.js
@@ -0,0 +1,19 @@
+console.log ("Hello")
+//let message; 
+//message = 'Hello'; //store the string hello in variable named message 
+let message = "Hello";
+alert(message); //shows the variable content
+
+let user = 'John';
+let age = 25;
+let greeting = "Hello";
+
+alert(greeting+ " " +user+ " "+age);
+
+let name = "John";
+let admin = name; 
+alert(admin);
+
+let ourPlanetName = 'Earth';
+let currentUserName = 'John';
+
