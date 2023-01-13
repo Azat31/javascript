c6e62030155ea80e5152a8cab137fc482cf965c7
Azat31 <929312w@gmail.com>
2023-01-13 03:35:03 -0500

    finish js fundamentals

---
+++ js/javascript.js
@@ -0,0 +1,28 @@
+/*let answer = parseInt(prompt("Please enter your number here"));
+for(let i = 1; i <= answer; i++) {
+    if(i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if(i % 5 == 0) {
+        console.log("Buzz");
+
+    } else {
+        console.log(i);
+    }
+
+}
+
+let answer = parseInt(prompt("pleaase enter any number")); 
+for(let i = 1; i<=answer; i++) {
+    if(i % 2 == 0) {
+        console.log("this is even number")
+    } else {
+        console.log("this number is odd")
+    }
+}
+*/
+const a = "Hello"
+const b = "World"
+
+console.log(c)
